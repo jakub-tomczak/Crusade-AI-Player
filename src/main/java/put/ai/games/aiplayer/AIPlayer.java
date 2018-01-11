a8391de8bf281660ea958cb0f12f2603157e28a4
Jakub Tomczak <jakub.tomczak@live.com>
2018-01-11 13:08:55 +0100

    Initial commit.

---
+++ src/main/java/put/ai/games/aiplayer/AIPlayer.java
@@ -0,0 +1,18 @@
+package put.ai.games.aiplayer;
+
+import put.ai.games.game.Board;
+import put.ai.games.game.Move;
+import put.ai.games.game.Player;
+
+public class AIPlayer extends Player {
+
+    @Override
+    public String getName() {
+        return "Player 127331,127083";
+    }
+
+    @Override
+    public Move nextMove(Board b) {
+        return null;
+    }
+}
