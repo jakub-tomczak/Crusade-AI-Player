3c736fede439d5682950ceaf0f2c5ca6e13871b4
Jakub Tomczak <jakub.tomczak@live.com>
2018-01-11 13:50:43 +0100

    Random result.

--- src/main/java/put/ai/games/aiplayer/AIPlayer.java
+++ src/main/java/put/ai/games/aiplayer/AIPlayer.java
@@ -4,15 +4,34 @@ import put.ai.games.game.Board;
 import put.ai.games.game.Move;
 import put.ai.games.game.Player;
 
+import java.util.List;
+import java.util.Random;
+
 public class AIPlayer extends Player {
 
     @Override
     public String getName() {
-        return "Player 127331,127083";
+        return "Player Konrad Kubzdela 127331 Jakub Tomczak 127083";
     }
 
     @Override
     public Move nextMove(Board b) {
-        return null;
+        List<Move> moves = b.getMovesFor(getColor());
+        Random random = new Random();
+        return moves.get(random.nextInt(moves.size()));
+    }
+
+    //private int alphaBeta(state s, int depth, int alpha, int beta)
+    private int alphaBeta(int depth, int alpha, int beta)
+    {
+        /*
+        int val;
+        if( is_terminal_node(s) || depth==0 ) return(Eval(s,depth));
+        for(child=1; child<=NumOfSucc(s); child++) {
+            val = -alphaBeta(Succ(s,child),depth-1,-beta,-alpha);
+            if( val > alpha ) alpha = val; // alpha=max(val,alpha);
+            if( alpha >= beta ) return beta; // cutoff
+        } //endfor*/
+        return alpha;
     }
 }
